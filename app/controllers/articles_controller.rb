32cb5f0ee172736bfcbf3d70ae4405b4a91b8ecd
tyamagu2 <tyamagu2@gmail.com>
2015-01-11 18:28:53 +0900

    Show tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.all.includes(:tags)
   end
 
   # GET /articles/1
